3980ff62c5d583d0d1ec7c188a4af623ed65dbf4
Luke <donovanbrady102@gmail.com>
2025-03-04 21:40:17 -0500

    Create index.ts

---
+++ src/core/index.ts
@@ -0,0 +1,9 @@
+console.log("NeoCord loaded!");
+const neoCord = {
+  version: "1.0.0",
+  init: () => {
+    console.log("Initializing NeoCord...");
+    // Add Discord patching logic here
+  }
+};
+neoCord.init();
